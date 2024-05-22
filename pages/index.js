d3f9e9f2b0fa2918dd27c15ca7f1f190ecd36311
soorya38 <csasoorya@gmail.com>
2024-05-22 22:16:34 +0530

    added features

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,61 @@ const printForm = () => {
     let header = document.getElementById('header');
 
     //company details
-    let name = docuement.getElementById('name');
-    let company = docuement.getElementById('company');
-    let gst = docuement.getElementById('gst');
+    let name = document.getElementById('name');
+    let company = document.getElementById('company');
+    let gst = document.getElementById('gst');
     let companyAdd = document.getElementById('companyAdd');
-    let city = docuement.getElementById('city');
-    let state = docuement.getElementById('state');
-    let country = docuement.getElementById('country');
+    let city = document.getElementById('city');
+    let state = document.getElementById('state');
+    let country = document.getElementById('country');
 
     //client details
-    let nameCl = docuement.getElementById('nameCl');
-    let companyCl = docuement.getElementById('companyCl');
-    let gstCl = docuement.getElementById('gstCl');
+    let nameCl = document.getElementById('nameCl');
+    let companyCl = document.getElementById('companyCl');
+    let gstCl = document.getElementById('gstCl');
     let companyAddCl = document.getElementById('companyAddCl');
-    let cityCl = docuement.getElementById('cityCl');
-    let stateCl = docuement.getElementById('stateCl');
-    let countryCl = docuement.getElementById('countryCl');
-    let placeOfSupply = docuement.getElementById('placeOfSupply');
+    let cityCl = document.getElementById('cityCl');
+    let stateCl = document.getElementById('stateCl');
+    let countryCl = document.getElementById('countryCl');
+    let placeOfSupply = document.getElementById('placeOfSupply');
 
     //invoice details
-    let invoiceNo = docuement.getElementById('invoiceNo');
-    let invoiceDate = docuement.getElementById('invoiceDate');
-    let dueDate = docuement.getElementById('dueDate');
+    let invoiceNo = document.getElementById('invoiceNo');
+    let invoiceDate = document.getElementById('invoiceDate');
+    let dueDate = document.getElementById('dueDate');
+
+    let doc = new jsPDF();
+
+    //comapany details
+    let y = 24;
+    doc.setFontStyle('bold');
+    doc.setFontSize(10);
+    doc.text(name.value, 18, y);
+    doc.setFontSize(14);
+    doc.setFontStyle('none');
+    doc.text(company.value, 18, y+=6);
+    doc.text(gst.value, 18, y+=6);
+    doc.text(companyAdd.value, 18, y+=6);
+    doc.text(city.value, 18, y+=6);
+    doc.text(state.value, 18, y+=6);
+
+    //header
+    doc.setFontSize(28);
+    doc.text(header.value, 140, 30);
+    doc.setFontSize(14);
+
+    y += 10
+    //client company details
+    doc.setFontSize(10);
+    doc.setFontStyle('bold');
+    doc.text("Bill to: ", 18, y+=6);
+    doc.setFontStyle('none');
+    doc.setFontSize(14);
+    doc.text(companyCl.value, 18, y+=6);
+    doc.text(gstCl.value, 18, y+=6);
+    doc.text(companyAddCl.value, 18, y+=6);
+    doc.text(cityCl.value, 18, y+=6);
+    doc.text(stateCl.value, 18, y+=6);
+
+    doc.save("test.pdf");
 }
