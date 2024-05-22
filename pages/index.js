86869ab6c30375b22ccd78dd443df5d7df25b1ca
soorya38 <csasoorya@gmail.com>
2024-05-22 16:14:12 +0530

    basic designs and js

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+const printForm = () => {
+    let header = document.getElementById('header');
+
+    //company details
+    let name = docuement.getElementById('name');
+    let company = docuement.getElementById('company');
+    let gst = docuement.getElementById('gst');
+    let companyAdd = document.getElementById('companyAdd');
+    let city = docuement.getElementById('city');
+    let state = docuement.getElementById('state');
+    let country = docuement.getElementById('country');
+
+    //client details
+    let nameCl = docuement.getElementById('nameCl');
+    let companyCl = docuement.getElementById('companyCl');
+    let gstCl = docuement.getElementById('gstCl');
+    let companyAddCl = document.getElementById('companyAddCl');
+    let cityCl = docuement.getElementById('cityCl');
+    let stateCl = docuement.getElementById('stateCl');
+    let countryCl = docuement.getElementById('countryCl');
+    let placeOfSupply = docuement.getElementById('placeOfSupply');
+
+    //invoice details
+    let invoiceNo = docuement.getElementById('invoiceNo');
+    let invoiceDate = docuement.getElementById('invoiceDate');
+    let dueDate = docuement.getElementById('dueDate');
+}
